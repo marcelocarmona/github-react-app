710d71fb25ddbd6186a6d0f81ec1e099a7ad9f5f
marcelocarmona <marcelo@marcelocarmona.com>
2017-05-04 01:54:42 -0300

    RepoList.css

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import './RepoList.css';
 
 export default function RepoList(props) {
   const listItem = props.repos.map(repo => (
     <li key={repo.id} className="repo-list">
-      <div>{repo.name}</div>
-      <div>{repo.language}</div>
-      <div>{repo.updated_at}</div>
+      <div className="repo-list__title">
+        <span className="repo-list__title__language">{repo.language}</span>
+        <span className="repo-list__title__updated-at">{getFormattedDate(repo.updated_at)}</span>
+      </div>
+      <div className="repo-list__name">{repo.name}</div>
+
     </li>
   ));
   return (<ul className="repos">{listItem}</ul>)
+}
+
+function getFormattedDate(date) {
+    var todayTime = new Date(date);
+    var month = todayTime.getMonth() + 1;
+    var day = todayTime.getDate();
+    var year = todayTime.getFullYear();
+    return day + "/" + month + "/" + year;
 }
